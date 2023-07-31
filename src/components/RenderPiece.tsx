88181d08c9cef18359c8274ffd87681f5d2d480c
capJavert <ante@kickass.website>
2023-07-31 02:24:19 +0200

    feat: add piece sounds

--- src/components/RenderPiece.tsx
+++ src/components/RenderPiece.tsx
@@ -1,4 +1,4 @@
-import { Piece, RenderPieceProps, pieceName } from '../types'
+import { Piece, PieceState, RenderPieceProps, pieceName } from '../types'
 import Pawn from '../assets/pieces/pawn.png'
 import Bishop from '../assets/pieces/bishop.png'
 import Knight from '../assets/pieces/knight.png'
@@ -7,6 +7,13 @@ import Queen from '../assets/pieces/queen.png'
 import King from '../assets/pieces/king.png'
 import styles from '../Game.module.css'
 import { composeCssClass } from '@kickass-coderz/utils'
+import { useAudio } from '../hooks'
+import pawnReadySound from '../assets/sounds/pawn.mp3'
+import knightReadySound from '../assets/sounds/knight.mp3'
+import queenReadySound from '../assets/sounds/queen.mp3'
+import bishopReadySound from '../assets/sounds/bishop.mp3'
+import rookReadySound from '../assets/sounds/rook.mp3'
+import kingReadySound from '../assets/sounds/king.mp3'
 
 const pieceImage: Record<Piece, string> = {
     [Piece.Pawn]: Pawn,
@@ -17,8 +24,19 @@ const pieceImage: Record<Piece, string> = {
     [Piece.King]: King
 }
 
+const pieceReadySound: Record<Piece, string> = {
+    [Piece.Pawn]: pawnReadySound,
+    [Piece.Bishop]: bishopReadySound,
+    [Piece.Knight]: knightReadySound,
+    [Piece.Rook]: rookReadySound,
+    [Piece.Queen]: queenReadySound,
+    [Piece.King]: kingReadySound
+}
+
 export const RenderPiece = ({ piece, state }: RenderPieceProps) => {
     const image = pieceImage[piece]
+    const readySoundSrc = pieceReadySound[piece]
+    const { play: playReadySound } = useAudio({ src: readySoundSrc })
 
     if (!image) {
         return null
@@ -26,9 +44,14 @@ export const RenderPiece = ({ piece, state }: RenderPieceProps) => {
 
     return (
         <img
-            className={composeCssClass(styles.pieceImage, styles[`pieceState-${state}`])}
+            className={composeCssClass(styles.pieceImage, styles[`pieceState-${state}`], styles[`piece-${piece}`])}
             src={image}
             alt={pieceName[piece]}
+            onClick={() => {
+                if (state === PieceState.Ready) {
+                    playReadySound({ loop: false })
+                }
+            }}
         />
     )
 }
